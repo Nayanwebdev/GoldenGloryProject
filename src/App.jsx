9d8bc936a2e146b5fc39d70f834448cb389e77b5
Nayanwebdev <nayanwebdevelopment1@gmail.com>
2023-03-29 11:35:25 +0530

    error page test

--- src/App.jsx
+++ src/App.jsx
@@ -11,13 +11,27 @@ import Cart from "./pages/Cart";
 import Profile from "./pages/Profile";
 import ScrollToTop from "./components/ScrollToTop";
 import Footer from "./components/Footer";
+import ErrorPage from "./components/ErrorPage.jsx";
+
+import { useState, useEffect } from "react";
+import { useParams } from "react-router";
 
 function App() {
+  const { id } = useParams();
+  const [show, setShow] = useState(true);
+  useEffect(() => {
+    setShow(true);
+  }, [id]);
+  const Error = (props) => {
+    useEffect(() => props.handleShow(false), []);
+    return <ErrorPage />;
+  };
+
   return (
     <>
       <GlobalStyle />
-      <Navbar />
       <ScrollToTop />
+      {show && <Navbar />}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about-us" element={<AboutUs />} />
@@ -27,8 +41,9 @@ function App() {
         <Route path="/liked" element={<Liked />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/*" element={<Error handleShow={(res) => setShow(false)} />} />
       </Routes>
-      <Footer />
+      {show && <Footer />}
     </>
   );
 }
---
+++ src/components/ErrorPage.jsx
@@ -0,0 +1,109 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
+import errImg from "../assets/images/error_Img.svg";
+
+export default function ErrorPage() {
+  return (
+    <Wrapper>
+      <div className="error-block-erp">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6 align-self-center">
+              <img src={errImg} alt="errImg" />
+            </div>
+            <div className="col-md-6 align-self-center">
+              <h1>404</h1>
+              <h2>UH OH! You're lost.</h2>
+              <p>The page you are looking for does not exist. How you got here is a mystery. But you can click the button below to go back to the homepage.</p>
+              <NavLink to="/" className="btn-erp dark">
+                Click here to go Home Page
+              </NavLink>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div`
+  .error-block-erp {
+    height: calc(100vh - 100px);
+    display: flex;
+    align-items: center;
+    text-align: center;
+    background: linear-gradient(90deg, rgba(208, 208, 208, 1) 48%, rgba(145, 145, 145, 1) 100%);
+    img {
+      width: 100%;
+    }
+    h1 {
+      font-size: 100px;
+      line-height: 110px;
+      margin-bottom: 15px;
+      font-weight: 800;
+    }
+    h2 {
+      font-size: 40px;
+      line-height: 50px;
+      font-weight: bold;
+    }
+    p {
+      padding-bottom: 20px;
+    }
+    .btn-erp {
+      z-index: 1;
+      overflow: hidden;
+      background: transparent;
+      position: relative;
+      padding: 8px 50px;
+      border-radius: 4px;
+      cursor: pointer;
+      font-size: 1em;
+      letter-spacing: 2px;
+      transition: 0.2s ease;
+      font-weight: bold;
+      margin: 5px 0px;
+      &.dark {
+        border: 2px solid var(--text-color);
+        color: var(--text-color);
+        &:before {
+          content: "";
+          position: absolute;
+          left: 0;
+          top: 0;
+          width: 0%;
+          height: 100%;
+          background: var(--text-color);
+          z-index: -1;
+          transition: 0.2s ease;
+        }
+        &:hover {
+          color: var(--white-color);
+          background: var(--text-color);
+          transition: 0.2s ease;
+          &:before {
+            width: 100%;
+          }
+        }
+      }
+    }
+  }
+  @media (min-width: 0px) and (max-width: 575px) {
+    .error-block-erp {
+      height: calc(100vh - 60px);
+      h1 {
+      font-size: 60px;
+      line-height: 70px;
+    }
+    }
+  }
+  @media (min-width: 576px) and (max-width: 767px) {
+  }
+  @media (min-width: 768px) and (max-width: 991px) {
+  }
+  @media (min-width: 992px) and (max-width: 1199px) {
+  }
+  @media (min-width: 1200px) and (max-width: 1440px) {
+  }
+`;
